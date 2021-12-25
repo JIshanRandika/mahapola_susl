2f622e1f99e3e33508e9db27596c289796d1a50a
JIshanRandika <jishanrandika@gmail.com>
2021-12-25 13:14:13 +0530

    helpers.php

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -16,13 +16,22 @@ function getMyPermission($id)
 {
     switch ($id) {
         case 1:
-            return 'admin';
+            return 'vice_chancellor';
             break;
         case 2:
-            return 'superadmin';
+            return 'registrar';
+            break;
+        case 3:
+            return 'assistant_registrar_of_the_faculty';
+            break;
+        case 4:
+            return 'student_affairs_division_clerk';
+            break;
+        case 5:
+            return 'finance_division_clerk';
             break;
         default:
-            return 'user';
+            return 'student';
             break;
     }
 }
