9102a6aa34b0ee1481a0539936d85e3a71da2080
JIshanRandika <jishanrandika@gmail.com>
2021-12-25 12:32:21 +0530

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $user = [
+            [
+                'name'=>'Admin',
+                'email'=>'admin@gmail.com',
+                'is_permission'=>'1',
+                'password'=> bcrypt('123456'),
+            ],
+            [
+                'name'=>'SuperAdmin',
+                'email'=>'superadmin@gmail.com',
+                'is_permission'=>'2',
+                'password'=> bcrypt('123456'),
+            ],
+            [
+                'name'=>'User',
+                'email'=>'user@gmail.com',
+                'is_permission'=>'0',
+                'password'=> bcrypt('123456'),
+            ],
+        ];
+
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware'=>'auth'], function () {
+    Route::get('permissions-all-users',['middleware'=>'check-permission:user|admin|superadmin','uses'=>'HomeController@allUsers']);
+    Route::get('permissions-admin-superadmin',['middleware'=>'check-permission:admin|superadmin','uses'=>'HomeController@adminSuperadmin']);
+    Route::get('permissions-superadmin',['middleware'=>'check-permission:superadmin','uses'=>'HomeController@superadmin']);
+});
