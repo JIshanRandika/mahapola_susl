160b072a81ff9d5dc23f285ab0e5f9632395978d
JIshanRandika <jishanrandika@gmail.com>
2021-12-25 21:08:05 +0530

    app.blade.php update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+{{--                    {{ config('app.name', 'Laravel') }}--}}
+                    SUSL
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -52,8 +53,8 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+{{--                            <li class="nav-item dropdown">--}}
+                                <a class="dropdown-item" href="#" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
@@ -68,7 +69,7 @@
                                         @csrf
                                     </form>
 {{--                                </div>--}}
-                            </li>
+{{--                            </li>--}}
                         @endguest
                     </ul>
                 </div>
